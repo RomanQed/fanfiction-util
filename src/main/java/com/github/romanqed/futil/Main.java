dd915332308f2d4000980db04f49ae8283e89fb3
RomanQed <gbakaldin@gmail.com>
2022-07-21 16:07:38 +0300

    Remove redundant main class

--- src/main/java/com/github/romanqed/futil/Main.java
+++
@@ -1,9 +0,0 @@
-package com.github.romanqed.futil;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(FanfictionUtilApplication.class, args);
-    }
-}
